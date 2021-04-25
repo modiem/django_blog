3709a53e29d78403db637b7ba163f8541512d95a
Mo Yang <mo_yang@foxmail.com>
2021-04-25 12:57:17 +0200

    sending email to reset the password

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_conplete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_conplete.html'), name='password_reset_complete'),
     path("", include("blog.urls"))
 ]
 
